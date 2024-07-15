74bdaad9f14731f8c61617649de606c3e86d025b
Trax <trax@elektronika.ba>
2024-07-15 15:52:36 +0200

    Update config.h

--- Inc/config.h
+++ Inc/config.h
@@ -501,7 +501,7 @@
   #undef  CTRL_MOD_REQ
   #define CTRL_MOD_REQ            VLT_MODE  // HOVERCAR works best in TORQUE Mode. VOLTAGE mode is preffered when freewheeling is not desired when throttle is released.
   #define CONTROL_ADC             0         // use ADC as input. Number indicates priority for dual-input. Disable CONTROL_SERIAL_USART2, FEEDBACK_SERIAL_USART2, DEBUG_SERIAL_USART2!
-  //#define SIDEBOARD_SERIAL_USART3 1         // Rx from right sensor board: to use photosensors as buttons. Number indicates priority for dual-input. Comment-out if sideboard is not used!
+  #define SIDEBOARD_SERIAL_USART3 1         // Rx from right sensor board: to use photosensors as buttons. Number indicates priority for dual-input. Comment-out if sideboard is not used!
   #define FEEDBACK_SERIAL_USART3            // Tx to   right sensor board: for LED battery indication. Comment-out if sideboard is not used!
 
   #define DUAL_INPUTS                       // ADC*(Primary) + Sideboard_R(Auxiliary). Uncomment this to use Dual-inputs
@@ -527,20 +527,16 @@
   #define MULTI_MODE_DRIVE                  // This option enables the selection of 3 driving modes at start-up using combinations of Brake and Throttle pedals (see below)
   #ifdef MULTI_MODE_DRIVE
       // BEGINNER MODE:     Power ON + Brake [released] + Throttle [released or pressed]
-      //#define MULTI_MODE_DRIVE_M1_MAX   175
-      //#define MULTI_MODE_DRIVE_M1_RATE  250
-      //#define MULTI_MODE_M1_I_MOT_MAX   4
-      //#define MULTI_MODE_M1_N_MOT_MAX   30
-      #define MULTI_MODE_DRIVE_M1_MAX   500  // INTERMEDIATE sam prekopirao, jer ovaj BEGINNER ne radi nikako...
+      #define MULTI_MODE_DRIVE_M1_MAX   500 // ADJUSTED
       #define MULTI_MODE_DRIVE_M1_RATE  300
       #define MULTI_MODE_M1_I_MOT_MAX   8
-      #define MULTI_MODE_M1_N_MOT_MAX   80
+      #define MULTI_MODE_M1_N_MOT_MAX   100
 
       // INTERMEDIATE MODE: Power ON + Brake [pressed] + Throttle [released]
-      #define MULTI_MODE_DRIVE_M2_MAX   500
-      #define MULTI_MODE_DRIVE_M2_RATE  300
-      #define MULTI_MODE_M2_I_MOT_MAX   8
-      #define MULTI_MODE_M2_N_MOT_MAX   80
+      #define MULTI_MODE_DRIVE_M2_MAX   750 // ADJUSTED
+      #define MULTI_MODE_DRIVE_M2_RATE  350
+      #define MULTI_MODE_M2_I_MOT_MAX   10
+      #define MULTI_MODE_M2_N_MOT_MAX   250
 
       // ADVANCED MODE:    Power ON + Brake [pressed] + Throttle [pressed]
       #define MULTI_MODE_DRIVE_M3_MAX   1000
